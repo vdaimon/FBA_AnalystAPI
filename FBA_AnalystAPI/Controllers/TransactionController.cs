c7413c7c3def52fde29f49d1288d6d4d63a0c833
d.vasiliev <d.vasiliev@mixvel.com>
2022-03-15 19:14:40 +0300

    Added responce methods for user balance base

--- FBA_AnalystAPI/Controllers/TransactionController.cs
+++ FBA_AnalystAPI/Controllers/TransactionController.cs
@@ -1,6 +1,7 @@
 ﻿using FBA_AnalystAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using RestClient.Net;
 
 namespace FBA_AnalystAPI.Controllers
 {
@@ -17,7 +18,7 @@ namespace FBA_AnalystAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Transaction>>> Get()
         {
-            return await db.Transactions.ToListAsync();
+            return await db.Transactions.OrderBy(x=>x.id).ToListAsync();
         }
 
         [HttpPost]
@@ -30,18 +31,72 @@ namespace FBA_AnalystAPI.Controllers
 
             db.Transactions.Add(transaction);
 
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex);
-            }
+            await db.SaveChangesAsync();
+
+            await SendNewBalance();
 
             return Ok(transaction);
         }
 
+        [HttpPut]
+        public async Task<ActionResult<User>> Put(Transaction transaction)
+        {
+            if (transaction == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Transactions.Any(x => x.id == transaction.id))
+            {
+                return NotFound();
+            }
+
+            db.Update(transaction);
+            await db.SaveChangesAsync();
+            await SendNewBalance();
+            return Ok(transaction);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Transaction? transaction = db.Transactions.FirstOrDefault(x => x.id == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+            db.Transactions.Remove(transaction);
+            await db.SaveChangesAsync();
+            await SendNewBalance();
+            return Ok(transaction);
+        }
+
+        private async Task<ActionResult> SendNewBalance()
+        {
+            using var client = new Client("https://localhost:7095");
+
+            await client.PostAsync<Balance, Balance>(CalculateBalance().Result, "api/balance");
+
+            return Ok();
+        }
+
+        private async Task<Balance> CalculateBalance()
+        {
+            decimal currentBalance = 0;
+
+            var trList = await db.Transactions.ToListAsync();
+            foreach (Transaction tr in trList)
+            {
+                if (tr.IsIncome)
+                    currentBalance += tr.Amount;
+                else currentBalance -= tr.Amount;
+            }
+
+            Balance balance = new Balance();
+            balance.DateTime = DateTime.UtcNow;
+            balance.Amount = currentBalance;
+
+            return balance;
+        }
 
     }
 }
---
+++ FBA_AnalystAPI/Models/Balance.cs
@@ -0,0 +1,9 @@
+﻿namespace FBA_AnalystAPI.Models
+{
+    public class Balance
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
